e3ed665eb464e6c14d525683f88d01d7b166d95f
Nathan Davidson <nd.bova@gmail.com>
2021-11-09 22:18:34 -0500

    Added additional test for counter

--- tests/counter_tests.ts
+++ tests/counter_tests.ts
@@ -22,4 +22,26 @@ describe('counter tests', () => {
     expect(histo.get(1)).to.equal(3);
     expect(histo.get(2)).to.equal(2);
   });
+
+  it('produces a total equal to the size of the input', () => {
+    const testArr: Array<number> = [];
+    const testLen: number = randInt(2048) + 1;
+    for (let i = 0; i < testLen; i++) {
+      testArr.push(randInt(2048));
+    }
+
+    const testIter: Iter<number> = new ArrayIter(testArr);
+    const histo: Map<number, number> = counter(testIter);
+
+    let histoValuesSum = 0;
+    for (const value of histo.values()) {
+      histoValuesSum += value;
+    }
+
+    expect(histoValuesSum).to.equal(testLen);
+  });
 });
+
+function randInt(ceil: number): number {
+  return Math.floor(Math.random() * ceil);
+}
